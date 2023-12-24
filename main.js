81674359e462a71f67977fe4a8f75dbc1dc3c2eb
Satyendra Upadhyay <satyendraupadya49@gmail.com>
2023-12-24 15:51:28 +0530

    2nd Commit - PutUpdateButWithIssue

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function getValue(e) {
       quantity: getQuantity.value
     }
 
-    axios.post('https://crudcrud.com/api/903a6e94e6dc425fbed6a6e083a0de44/candy', candyDetails)
+    axios.post('https://crudcrud.com/api/b46cdf29eb8a4e60949027aa0ce78ea3/candy', candyDetails)
     .then(res => console.log(res))
     .catch(err => console.log(err))
     candyQuantities[candyDetails.candyname] = candyDetails.quantity;
@@ -30,7 +30,7 @@ function getValue(e) {
 function displaySavedData(userDetails) {
     var listItem = document.createElement('li');
     var candyName = userDetails.candyname;
-    listItem.textContent = `${userDetails.candyname} - ${userDetails.description} - ${userDetails.price} -  `;
+    listItem.textContent = `${userDetails.candyname} - ${userDetails.description} - ${userDetails.price} - `;
     listItem.classList.add('p-2');
 
     var quantityDisplay = document.createElement('span');
@@ -38,31 +38,52 @@ function displaySavedData(userDetails) {
     quantityDisplay.setAttribute('data-candy', candyName);
     listItem.appendChild(quantityDisplay);
 
-    var btn1 = createBuyButton('Buy 1', candyName);
-    var btn2 = createBuyButton('Buy 2', candyName);
-    var btn3 = createBuyButton('Buy 3', candyName);
+    var btn1 = createBuyButton('Buy 1', candyName, userDetails);
+    var btn2 = createBuyButton('Buy 2', candyName, userDetails);
+    var btn3 = createBuyButton('Buy 3', candyName, userDetails);
+    
+    // var del = document.createElement('button');
+    // button.type = "button";
+    // button.textContent = 'Delete';
+    // button.className = 'btn btn-danger btn-sm mr-2';
 
     candyList.appendChild(listItem);
     listItem.appendChild(btn1);
     listItem.appendChild(btn2);
     listItem.appendChild(btn3);
-
-
+    // listItem.appendChild(del);
     
 }
 
-function createBuyButton(text, candyName) {
+
+function createBuyButton(text, candyName, userDetails) {
     var button = document.createElement('button');
     button.type = "button";
     button.textContent = text;
     button.className = 'btn btn-primary btn-sm mr-2';
 
     button.addEventListener('click', function () {
-        decreaseQuantity(candyName, parseInt(text.split(' ')[1])); // Extract the number from button text
+        var quantityToBuy = parseInt(text.split(' ')[1]);
+        var updatedQuantity = decreaseQuantity(candyName, quantityToBuy);
 
-        axios.put('https://crudcrud.com/api/903a6e94e6dc425fbed6a6e083a0de44/candy')
-         .then(res => console.log(res))
-         .catch(err => console.log(err))
+
+        var itemId = userDetails._id;
+
+        const updatedData = {
+            name: userDetails.candyname,
+            description: userDetails.description,
+            price: userDetails.price,
+            quantity: updatedQuantity
+        };
+    
+        axios.put(`https://crudcrud.com/api/b46cdf29eb8a4e60949027aa0ce78ea3/candy/${itemId}`, updatedData)
+            .then(res => {
+                console.log('Item Updated:', res.data);
+            })
+            .catch(err => {
+                console.error('Error Updating item:', err);
+            });
+
     });
 
     return button;
@@ -78,8 +99,10 @@ function decreaseQuantity(candyName, amount) {
             var newQuantity = currentQuantity - amount;
             quantityElement.textContent = newQuantity;
             candyQuantities[candyName] = newQuantity;
+            return newQuantity;
         }
     }
+    // return currentQuantity;
 }
 
 function findListItemByCandyName(candyName) {
@@ -92,3 +115,17 @@ function findListItemByCandyName(candyName) {
     }
     return null;
 }
+
+ // GET the saved User Details from crudcrud.
+ window.addEventListener("DOMContentLoaded", () => {
+    axios.get("https://crudcrud.com/api/b46cdf29eb8a4e60949027aa0ce78ea3/candy")
+    .then(res => {
+        for(var i = 0; i < res.data.length;i++){
+            displaySavedData(res.data[i])
+            
+        }
+        console.log(res);
+    })
+    .catch(err => console.error(err));
+
+})
